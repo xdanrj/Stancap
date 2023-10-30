7cdc2f9b7090729ce67b9ab2064b72f236214b48
Danilo Carvalho <xdanrj@gmail.com>
2023-10-30 15:15:52 -0300

    30/10/23 15h15

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,14 +1,28 @@
 import { React, useState } from "react";
 import { Form, FormGroup, FloatingLabel, Row, Col, Button } from "react-bootstrap";
-import { MDBInput, MDBIcon, MDBBtn } from 'mdb-react-ui-kit';
+import { MDBInput, MDBIcon, MDBBtn, MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem } from 'mdb-react-ui-kit';
 import { MDBInputGroup } from "./SearchBarStyles";
 
 export function SearchBar() {
     return (
         <>  
             <MDBInputGroup>
+            <MDBDropdown>
+            <MDBDropdownToggle size="lg" color="dark">Tipo</MDBDropdownToggle>
+                    <MDBDropdownMenu dark>
+                        <MDBDropdownItem link>Autor</MDBDropdownItem>
+                        <MDBDropdownItem>Tag</MDBDropdownItem>
+                        <MDBDropdownItem>Source</MDBDropdownItem>
+                        <MDBDropdownItem>Upload por</MDBDropdownItem>
+                        <MDBDropdownItem>Contexto</MDBDropdownItem>
+                        <MDBDropdownItem divider />
+                        <MDBDropdownItem></MDBDropdownItem>
+                    </MDBDropdownMenu>
+                </MDBDropdown>
+            
                 <MDBInput className="" contrast label='Pesquise' />
-                <MDBBtn rippleColor='white' onClick={() => a}>
+                
+                <MDBBtn color="dark" rippleColor='white' onClick={() => a}>
                     <MDBIcon color="white" icon='search' />
                 </MDBBtn>
             </MDBInputGroup>
