61f42f254f3269740a9f3dacd00f0f967bea9e01
Danilo Carvalho <xdanrj@gmail.com>
2023-11-01 20:11:49 -0300

    01/11/23 20h10

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -4,6 +4,10 @@ import { InputGroup } from "./SearchBarStyles";
 import DropdownItem from "react-bootstrap/esm/DropdownItem";
 import { MDBIcon } from "mdb-react-ui-kit";
 
+export function useSearch(searchString) {
+    return searchString
+}
+
 export function SearchBar() {
     const SearchTypes = [
         { label: "Autor", value: "author" },
@@ -14,26 +18,33 @@ export function SearchBar() {
     ]
 
     const [selectedType, setSelectedType] = useState()
+    const [searchString, setSearchString] = useState("")
 
     const handleTypeSelect = (eventKey) => {
         console.log(eventKey)
         console.log(SearchTypes[eventKey])
         setSelectedType(SearchTypes[eventKey])
-        
+    }
+
+
+
+    const handleSearchChange = (e) => {
+        const { name, value } = e.target
+        setSearchString(value)
     }
 
     return (
         <>
             <InputGroup>
-            <DropdownButton variant="dark" menuVariant="dark" title={selectedType ? selectedType.label : "Tipo"} onSelect={handleTypeSelect}>
-            {SearchTypes.map((item, index) => (
-                <DropdownItem eventKey={index} key={item.value}>{item.label}</DropdownItem>
-            ))}
-            </DropdownButton>
-            <Form.Control placeholder="Pesquise..." />
-            <Button variant="dark">
-                <MDBIcon icon="search"/>
-            </Button>
+                <DropdownButton variant="dark" menuVariant="dark" title={selectedType ? selectedType.label : "Tipo"} onSelect={handleTypeSelect}>
+                    {SearchTypes.map((item, index) => (
+                        <DropdownItem eventKey={index} key={item.value}>{item.label}</DropdownItem>
+                    ))}
+                </DropdownButton>
+                <Form.Control placeholder="Pesquise..." onChange={handleSearchChange} />
+                <Button variant="dark" /*onClick={useSearch(searchString)}*/>
+                    <MDBIcon icon="search" />
+                </Button>
             </InputGroup>
         </>
     )
