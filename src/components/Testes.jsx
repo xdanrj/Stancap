73c963bd93f882797aa636f0a0e29d1781874b4e
xdanrj <xdanrj@gmail.com>
2024-02-19 10:17:24 -0300

    19/02/24 10h15

--- src/components/Testes.jsx
+++ src/components/Testes.jsx
@@ -3,25 +3,32 @@ import React from "react"
 import { useState } from "react"
 
 export default function Testes() {
-    const [item, setItem] = useState(["a"])
+    //const [data, setData] = useState({})
+    let data = {}
+    const regex = /]\s*([^:]+):\s*([^]+)(?=\s*\[|$)/
 
-    const handleClick = () => {
-        setItem(item => [...item, "a"])
+    const text = `[5/2 13:45] Chev²🦆: Assim dado momento 
+    [5/2 13:46] Chev²🦆: E é só questão de tempo até superar a oferta
+    [5/2 13:46] Danilo: Logo a terrestre
+    [5/2 13:46] Kayo Gameplaisu II: Mecânica do `
+
+    // Iterar sobre todas as correspondências usando exec()
+    let match
+    while ((match = regex.exec(text)) !== null) {
+        // match[1] contém o nome e match[2] contém o texto
+        const name = match[1]
+        const value = match[2]
+
+        // Adicionar a chave e valor ao objeto
+        data[name] = value.trim()
     }
 
-    return (
-        <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', alignItems: 'flex-start' }}>
-            <Button onClick={handleClick}>+1</Button>
-            <div className="bg-primary">
-                <p>div superior fixa</p>
-            </div>
+    // Imprimir o objeto resultante no console para verificação
+    console.log(data)
 
-            {item.map((item, index) => (
-                <div key={index} className="bg-info" >
-                    <p>{item}</p>
-                </div>
-            ))
-            }
+    return (
+        <div>
+            <p>{data}</p>
         </div>
     )
 }
