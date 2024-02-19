e6804eb741d12fd287d407639980f8c131d22a06
xdanrj <xdanrj@gmail.com>
2024-02-19 11:43:25 -0300

    19/02/24 11h45

--- src/components/Testes.jsx
+++ src/components/Testes.jsx
@@ -1,31 +1,32 @@
-import { Button } from "react-bootstrap"
-import React from "react"
-import { useState } from "react"
+import { Button } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
 
 export default function Testes() {
-    const text = `
-[5/2 13:45] Chev²: Assim dado momento
-[5/2 13:46] Chev²: E é só questão de tempo até superar a oferta
+  const [data, setData] = useState([]);
+  const text = `
+[5/2 13:45] Chev: Assim dado momento
+[5/2 13:46] Chev: E é só questão de tempo até superar a oferta
 [5/2 13:46] Danilo: Logo a terrestre
 [5/2 13:46] Kayo Gameplaisu II: Mecânica do
 `;
 
-    const regex = /(?<=\]\s)(.*?)(?=\s:)/g
-
+  useEffect(() => {
+    const regex = /\(?<=\]\s)(.*?)(?=\s:)/g;
     const matches = text.matchAll(regex);
 
-    const objects = Array.from(matches, match => {
-        return {
-            key: match[1],
-            value: match[2],
-        };
-    });
+    const objects = Array.from(matches, (match) => ({
+      key: match[1],
+      value: match[2], 
+    }));
 
-    console.log(objects);
+    setData(objects);
+  }, []);
 
-    return (
-        <div>
-            <p></p>
-        </div>
-    )
+  console.log(data)
+
+  return (
+    <div>
+      <Button onClick={() => console.log(data)}>clica</Button>
+    </div>
+  );
 }
