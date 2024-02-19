f77358a3e3ec559cff6219da1fd05df82e9f280f
xdanrj <xdanrj@gmail.com>
2024-02-19 18:11:15 -0300

    19/02/24 18h10

--- src/components/Testes.jsx
+++ src/components/Testes.jsx
@@ -2,34 +2,21 @@ import { Button } from "react-bootstrap"
 import React, { useEffect, useState } from "react"
 
 export default function Testes() {
-
     const [data, setData] = useState([])
     const text = `
-[5/2 13:45] Chev: Assim dado momento
-[5/2 13:46] Chev: E é só questão de tempo até superar a oferta
-[5/2 13:46] Danilo: Logo a terrestre
-[5/2 13:46] Kayo Gameplaisu II: Mecânica do
+[5/2 16:35] Joao: Lorem ipsum dolor sit amet
+[8/3 21:26] Chev: ullamco laboris nisi ut aliquip
+[7/2 14:04] Danilo: deserunt mollit anim id est laborum
+[9/1 03:96] Kayo Gameplaisu II: quis nostrum exercitationem
 `
-
+    const regex = /\b(\w+)\s*:/
+    let arrayResult
     // first match everything after ] until : then match everything after : until [ then repeats
-
     useEffect(() => {
-        const regex = /[abc]+/g
-        const matches = text.matchAll(regex);
-        console.log(matches)
-        for (const match of matches) {
-            console.log(match)
+        while((arrayResult = regex.exec(text)) !== null) {
+            console.log(arrayResult[0])
         }
-        const objects = Array.from(matches, match => {
-            return {
-                key: match[1],
-                value: match[2],
-            }
-        })
-
-        setData(objects)
     }, [])
-
 
     return (
         <div>
