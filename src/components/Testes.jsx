ecf06b6db7fded3001e3614253ecfa5463b55235
xdanrj <xdanrj@gmail.com>
2024-02-19 18:53:59 -0300

    19/02/24 18h53 alegriiiiiia

--- src/components/Testes.jsx
+++ src/components/Testes.jsx
@@ -9,13 +9,31 @@ export default function Testes() {
 [7/2 14:04] Danilo: deserunt mollit anim id est laborum
 [9/1 03:96] Kayo Gameplaisu II: quis nostrum exercitationem
 `
-    const regex = /\b(\w+)\s*:/
-    let arrayResult
     // first match everything after ] until : then match everything after : until [ then repeats
+    const regexChave = /]\s*([\w\s]+)\s*:/g;
+    const regexValor = /:\s*(\w+(?:\s+\w+)*)\s*$/;
+    let arrayResult
+
     useEffect(() => {
-        while((arrayResult = regex.exec(text)) !== null) {
-            console.log(arrayResult[0])
-        }
+        const regexChave = /]\s*([\w\s]+)\s*:/;
+        const regexValor = /:\s*(\w+(?:\s+\w+)*)\s*$/;
+
+        const linhas = text.trim().split('\n');
+        const objetos = [];
+
+        linhas.forEach((linha) => {
+            const chaveMatch = regexChave.exec(linha);
+            const valorMatch = regexValor.exec(linha);
+
+            if (chaveMatch && valorMatch) {
+                const chave = chaveMatch[1];
+                const valor = valorMatch[1];
+                const objeto = { [chave]: valor };
+                objetos.push(objeto);
+            }
+        });
+
+        console.log(objetos);
     }, [])
 
     return (
