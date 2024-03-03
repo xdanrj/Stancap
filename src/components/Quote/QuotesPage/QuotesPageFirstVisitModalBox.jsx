03656b25f8c5f5ee47b0819fea5c02e2a3358bd1
xdanrj <xdanrj@gmail.com>
2024-03-03 20:15:04 -0300

    03/03/24 20h15

--- src/components/Quote/QuotesPage/QuotesPageFirstVisitModalBox.jsx
+++ src/components/Quote/QuotesPage/QuotesPageFirstVisitModalBox.jsx
@@ -6,7 +6,7 @@ import _ from "lodash"
 
 export function QuotesPageFirstVisitModalBox(props) {
     const [show, setShow] = useState(true)
-    const [logoIdx, setLogoIdx] = useState(SourceNames[0])
+    const [logoIndex, setLogoIndex] = useState(0)
     const handleClose = () => {
         setShow(false)
         localStorage.setItem("hadVisitedQuotesPageBefore", true)
@@ -15,12 +15,13 @@ export function QuotesPageFirstVisitModalBox(props) {
     useEffect(() => {
         if (show) {
             const interval = setInterval(() => {
-                setLogoIdx((prevIdx) => SourceNames[(prevIdx + 1) % SourceNames.length])
-                console.log();
+                setLogoIndex(prevIndex => (prevIndex + 1) % SourceNames.length)
+                console.log(logoIndex)
             }, 1000)
             return () => clearInterval(interval)
         }
     }, [show])
+    
 
 
     return (
@@ -33,7 +34,7 @@ export function QuotesPageFirstVisitModalBox(props) {
                 <ModalBody>
                     <p>Não leve nada daqui a sério. Todas as frases são para fins unicamente cômicos</p>
                     <p>Clique em qualquer ícone de source ou no autor da quote para fazer uma pesquisa específica</p>
-                    <img src={`/images/${logoIdx?.value}.png`}></img>
+                    <img src={`/images/${SourceNames[logoIndex]}.png`}></img>
                     <p>Alguns dialógos são grandes então clique em ᨆ para expandir</p>
 
                     <Button onClick={handleClose}>Entendi</Button>
