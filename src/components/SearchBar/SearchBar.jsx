21542ddd931b201fe8e8f577c51b4568bfe53450
xdanrj <xdanrj@gmail.com>
2024-03-21 19:52:37 -0300

    19h50

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,6 @@
 import { React, useState, useEffect } from "react";
-import { Form, Button, DropdownButton, Row, Col, Container } from "react-bootstrap";
+import { Form, Button, DropdownButton, DropdownItem, Row, Col, Container, ButtonGroup, ToggleButtonGroup, ToggleButton } from "react-bootstrap";
 import { InputGroup } from "./SearchBarStyles";
-import DropdownItem from "react-bootstrap/esm/DropdownItem";
 import { MDBIcon } from "mdb-react-ui-kit";
 import { useAlertMsg } from "../Alert/AlertContext";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -14,7 +13,8 @@ export function SearchBar({ fetchAllQuotes, searchQuery, setSearchQuery }) {
     const location = useLocation()
     const navigate = useNavigate()
     const [searchParams, setSearchParams] = useSearchParams()
-    const [selectedType, setSelectedType] = useState()
+    const [selectedSearchType, setSelectedSearchType] = useState()
+    const [selectedQuoteType, setSelectedQuoteType] = useState(searchParams.get("type") ? searchParams.get("type") : "single")
     const [typeColor, setTypeColor] = useState(false)
     const [inputColor, setInputColor] = useState(false)
     const [searchTypes, setSearchTypes] = useState([
@@ -50,7 +50,7 @@ export function SearchBar({ fetchAllQuotes, searchQuery, setSearchQuery }) {
             async function settingQuery() {
                 const foundType = searchTypes.find((type) => type.value === Object.keys(queryString)[0])
                 console.log(foundType)
-                setSelectedType(foundType)
+                setSelectedSearchType(foundType)
                 setSearchQuery((prevSearchQuery) => ({
                     ...prevSearchQuery,
                     "query": queryString,
@@ -78,12 +78,12 @@ export function SearchBar({ fetchAllQuotes, searchQuery, setSearchQuery }) {
     }, [searchQuery])
 
     const handleTypeSelect = (eventKey) => {
-        setSelectedType(searchTypes.find((type) => type.value === eventKey))
+        setSelectedSearchType(searchTypes.find((type) => type.value === eventKey))
     }
 
     const handleSourceSelect = async (eventKey) => {
         console.log(eventKey)
-        setSelectedType({ label: "Source", value: "source" })
+        setSelectedSearchType({ label: "Source", value: "source" })
 
         await setSearchQuery({ "query": { "source": eventKey }, "label": "Source" })
 
@@ -91,19 +91,25 @@ export function SearchBar({ fetchAllQuotes, searchQuery, setSearchQuery }) {
         console.log("uuuuu")
     }
 
+    const handleQuoteTypeSelect = (value) => {
+        setSelectedQuoteType(value)
+        searchParams.set("type", value)
+        navigate({ search: searchParams.toString() })
+    }
+
     const handleSearchChange = (e) => {
         setSearchQuery((prevSearchQuery) => ({
             ...prevSearchQuery,
-            query: { [selectedType?.value]: e.target.value },
-            label: selectedType?.label
+            query: { [selectedSearchType?.value]: e.target.value },
+            label: selectedSearchType?.label
         }))
     }
 
     const checkAttributes = () => {
-        if (!selectedType) {
+        if (!selectedSearchType) {
             setTypeColor(true)
             setTimeout(() => { setTypeColor(false) }, 500)
-        } else if (!searchQuery.query[selectedType.value]) {
+        } else if (!searchQuery.query[selectedSearchType.value]) {
             setInputColor(true)
             setTimeout(() => { setInputColor(false) }, 500)
         } else {
@@ -122,21 +128,26 @@ export function SearchBar({ fetchAllQuotes, searchQuery, setSearchQuery }) {
 
     return (
         <>
-            {!(selectedType?.value === "source") && (
+            {!(selectedSearchType?.value === "source") && (
                 <Row className="justify-content-center">
                     <Col md={8} lg={5}>
                         <InputGroup >
-                            <DropdownButton variant={typeColor ? "danger" : "dark"} menuVariant="dark" title={selectedType ? selectedType.label : "Tipo"} onSelect={handleTypeSelect}>
+                            <DropdownButton variant={typeColor ? "danger" : "dark"} menuVariant="dark" title={selectedSearchType ? selectedSearchType.label : "Tipo"} onSelect={handleTypeSelect}>
                                 {searchTypes.map((item, index) => (
                                     <DropdownItem eventKey={item.value} key={item.value}>{item.label}</DropdownItem>
                                 ))}
+                                <ToggleButtonGroup type="radio" name="quotetype" value={selectedQuoteType}
+                                 onChange={(value)=> handleQuoteTypeSelect(value)}>
+                                    <ToggleButton id="single" value={"single"} size="sm">Citação</ToggleButton>
+                                    <ToggleButton id="multiple" value={"multiple"} size="sm">Diálogo</ToggleButton>
+                                </ToggleButtonGroup>
                             </DropdownButton>
 
                             <>
                                 <Form.Control
                                     className={inputColor ? "bg-danger" : "bg-light"}
-                                    placeholder={selectedType?.value === "tags" ? "Separe as tags por vírgula" : "Pesquise..."} onChange={handleSearchChange}
-                                    value={searchQuery?.query[selectedType?.value] || ""}
+                                    placeholder={selectedSearchType?.value === "tags" ? "Separe as tags por vírgula" : "Pesquise..."} onChange={handleSearchChange}
+                                    value={searchQuery?.query[selectedSearchType?.value] || ""}
                                 />
 
                                 <Button variant="dark" onClick={() => checkAttributes() ? handleSearchClick() : null}>
@@ -150,11 +161,11 @@ export function SearchBar({ fetchAllQuotes, searchQuery, setSearchQuery }) {
                     </Col>
                 </Row>
             )}
-            {selectedType?.value === "source" && (
+            {selectedSearchType?.value === "source" && (
                 <Row className="justify-content-center">
                     <Col md={8} lg={5}>
                         <InputGroup className="d-flex justify-content-center">
-                            <DropdownButton variant={typeColor ? "danger" : "dark"} menuVariant="dark" title={selectedType ? selectedType.label : "Tipo"} onSelect={handleTypeSelect}>
+                            <DropdownButton variant={typeColor ? "danger" : "dark"} menuVariant="dark" title={selectedSearchType ? selectedSearchType.label : "Tipo"} onSelect={handleTypeSelect}>
 
                                 {searchTypes.map((item, index) => (
                                     <DropdownItem eventKey={item.value} key={item.value}>{item.label}</DropdownItem>
