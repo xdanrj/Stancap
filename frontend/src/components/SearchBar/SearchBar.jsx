26e7f705bc484cdc66c5a566d1eb1ecebadf845b
xdanrj <xdanrj@gmail.com>
2024-04-08 09:39:39 -0300

    9h40

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -19,7 +19,7 @@ export function SearchBar({ fetchAllQuotes }) {
     const [selectedQuoteType, setSelectedQuoteType] = useState(searchParams.get("quoteType") || null)
     const [typeColor, setTypeColor] = useState(false)
     const [inputColor, setInputColor] = useState(false)
-    const [searchQuery, setSearchQuery] = useState({ "query": {}, "label": "" })
+    //const [searchQuery, setSearchQuery] = useState({ "query": {}, "label": "" })
     const [searchTypes, setSearchTypes] = useState(QuotesProperties)
 
     useEffect(() => {
@@ -36,39 +36,22 @@ export function SearchBar({ fetchAllQuotes }) {
             setSearchTypes(searchTypes)
         }
 
-        let queryString = {}
-        for (let param of searchParams) {
-            if (param[0] !== "page" && param[0] !== "sort") {
-                queryString[param[0]] = param[1]
+        let propertyQuery = {}
+        for (let [key, value] of searchParams) {
+            if (!(key === "page" || key === "sort" || key === "quoteType")) {
+                propertyQuery[key] = value
             }
         }
-
-        if (Object.keys(queryString).length > 0) {
-            async function settingQuery() {
-                const foundType = searchTypes.find((type) => type.value === Object.keys(queryString)[0])
-                setSelectedSearchType(foundType)
-                setSearchQuery((prevSearchQuery) => ({
-                    ...prevSearchQuery,
-                    "query": queryString,
-                    "label": foundType?.label
-                }))
-            }
-            settingQuery()
+        console.log(propertyQuery)
+        if (propertyQuery.length > 0) {
+                //                                                          Object.keys(propertyQuery)[0]
+                const foundType = searchTypes.find((type) => type.value === propertyQuery)
+                setSelectedSearchType(foundType)                
         } else {
             fetchAllQuotes ? fetchAllQuotes() : null
         }
     }, [])
 
-    useEffect(() => {
-        console.log(searchQuery)
-        for (const key of searchParams.keys()) {
-            searchParams.delete(key)
-        }
-        for (const [key, value] of Object.entries(searchQuery.query)) {
-            searchParams.set(key, value)
-        }
-        
-    }, [searchQuery])
 
     useEffect(() => {
         navigate({ search: searchParams.toString() })
@@ -78,10 +61,11 @@ export function SearchBar({ fetchAllQuotes }) {
         setSelectedSearchType(searchTypes.find((type) => type.value === eventKey))
     }
 
-    const handleSourceSelect = async (eventKey) => {        
+    const handleSourceSelect = async (eventKey) => {
         console.log(eventKey)
         setSelectedSearchType({ label: "Source", value: "source" })
-        setSearchQuery({ "query": { "source": eventKey }, "label": "Source" })        
+        //setSearchQuery({ "query": { "source": eventKey }, "label": "Source" })
+        searchParams.set("source", eventKey)
         navigate({ search: searchParams.toString() })
     }
 
@@ -92,11 +76,12 @@ export function SearchBar({ fetchAllQuotes }) {
     }
 
     const handleSearchChange = (e) => {
-        setSearchQuery((prevSearchQuery) => ({
-            ...prevSearchQuery,
-            query: { [selectedSearchType?.value]: e.target.value },
-            label: selectedSearchType?.label
-        }))
+        // setSearchQuery((prevSearchQuery) => ({
+        //     ...prevSearchQuery,
+        //     query: { [selectedSearchType?.value]: e.target.value },
+        //     label: selectedSearchType?.label
+        // }))
+        searchParams.set()
     }
 
     const checkAttributes = () => {
@@ -128,80 +113,80 @@ export function SearchBar({ fetchAllQuotes }) {
 
     return (
         <>
-        <div style={{"marginBottom": "-2rem"}}>
-            {!(selectedSearchType?.value === "source") && (
-                <Row className="justify-content-center">
-                    <Col xs={12} md={8} lg={5}>
-                        <InputGroup >
-                            <DropdownButton variant={typeColor ? "danger" : "dark"} menuVariant="dark" title={selectedSearchType ? selectedSearchType.label : "Tipo"} onSelect={handleTypeSelect}>
-                                {searchTypes.map((item, index) => (
-                                    <DropdownItem eventKey={item.value} key={item.value}>{item.label}</DropdownItem>
-                                ))}
-                                <ToggleButtonGroup type="radio" name="quoteType" value={selectedQuoteType}
-                                    onChange={(value) => handleQuoteTypeSelect(value)}>
-                                    <ToggleButton id="single" value={"single"} size="sm">Citação</ToggleButton>
-                                    <ToggleButton id="multiple" value={"multiple"} size="sm">Diálogo</ToggleButton>
-                                </ToggleButtonGroup>
-                            </DropdownButton>
+            <div style={{ "marginBottom": "-2rem" }}>
+                {!(selectedSearchType?.value === "source") && (
+                    <Row className="justify-content-center">
+                        <Col xs={12} md={8} lg={5}>
+                            <InputGroup >
+                                <DropdownButton variant={typeColor ? "danger" : "dark"} menuVariant="dark" title={selectedSearchType ? selectedSearchType.label : "Tipo"} onSelect={handleTypeSelect}>
+                                    {searchTypes.map((item, index) => (
+                                        <DropdownItem eventKey={item.value} key={item.value}>{item.label}</DropdownItem>
+                                    ))}
+                                    <ToggleButtonGroup type="radio" name="quoteType" value={selectedQuoteType}
+                                        onChange={(value) => handleQuoteTypeSelect(value)}>
+                                        <ToggleButton id="single" value={"single"} size="sm">Citação</ToggleButton>
+                                        <ToggleButton id="multiple" value={"multiple"} size="sm">Diálogo</ToggleButton>
+                                    </ToggleButtonGroup>
+                                </DropdownButton>
 
-                            <>
-                                <Form.Control
-                                    className={inputColor ? "bg-danger" : "bg-light"}
-                                    placeholder={selectedSearchType?.value === "tags" ? "Separe as tags por vírgula" : "Pesquise..."} onChange={handleSearchChange}
-                                    value={searchQuery?.query[selectedSearchType?.value] || ""}
-                                />
+                                <>
+                                    <Form.Control
+                                        className={inputColor ? "bg-danger" : "bg-light"}
+                                        placeholder={selectedSearchType?.value === "tags" ? "Separe as tags por vírgula" : "Pesquise..."} onChange={handleSearchChange}
+                                        value={searchQuery?.query[selectedSearchType?.value] || ""}
+                                    />
 
-                                <Button variant="dark" onClick={() => handleClearSearch()}>
-                                    <MDBIcon fas icon="times" />
+                                    <Button variant="dark" onClick={() => handleClearSearch()}>
+                                        <MDBIcon fas icon="times" />
+                                    </Button>
+
+                                </>
+                                <Button variant="dark" onClick={() => handleSortChange()}><i className=
+                                    {searchParams.get("sort") === "ascending" ? "bi bi-sort-down-alt" : "bi bi-sort-up-alt"}>
+                                </i>
                                 </Button>
 
-                            </>
-                            <Button variant="dark" onClick={() => handleSortChange()}><i className=
-                                {searchParams.get("sort") === "ascending" ? "bi bi-sort-down-alt" : "bi bi-sort-up-alt"}>
-                            </i>
-                            </Button>
+                                <Button onClick={() => checkAttributes() ? handleSearchClick() : null}>
+                                    <MDBIcon icon="search" />
+                                </Button>
+                            </InputGroup>
+                        </Col>
+                    </Row>
+                )}
+                {selectedSearchType?.value === "source" && (
+                    <>
+                        <Row className="justify-content-center">
+                            <Col md={8} lg={5}>
+                                <InputGroup className="d-flex justify-content-center">
+                                    <DropdownButton variant={typeColor ? "danger" : "dark"} menuVariant="dark" title={selectedSearchType ? selectedSearchType.label : "Tipo"} onSelect={handleTypeSelect}>
 
-                            <Button onClick={() => checkAttributes() ? handleSearchClick() : null}>
-                                <MDBIcon icon="search" />
-                            </Button>
-                        </InputGroup>
-                    </Col>
-                </Row>
-            )}
-            {selectedSearchType?.value === "source" && (
-                <>
-                <Row className="justify-content-center">
-                    <Col md={8} lg={5}>
-                        <InputGroup className="d-flex justify-content-center">
-                            <DropdownButton variant={typeColor ? "danger" : "dark"} menuVariant="dark" title={selectedSearchType ? selectedSearchType.label : "Tipo"} onSelect={handleTypeSelect}>
+                                        {searchTypes.map((item) => (
+                                            <DropdownItem eventKey={item.value} key={item.value}>{item.label}</DropdownItem>
+                                        ))}
+                                        <ToggleButtonGroup type="radio" name="quoteType" value={selectedQuoteType}
+                                            onChange={(value) => handleQuoteTypeSelect(value)}>
+                                            <ToggleButton id="single" value={"single"} size="sm">Citação</ToggleButton>
+                                            <ToggleButton id="multiple" value={"multiple"} size="sm">Diálogo</ToggleButton>
+                                        </ToggleButtonGroup>
+                                    </DropdownButton>
 
-                                {searchTypes.map((item, index) => (
-                                    <DropdownItem eventKey={item.value} key={item.value}>{item.label}</DropdownItem>
-                                ))}
-                                <ToggleButtonGroup type="radio" name="quoteType" value={selectedQuoteType}
-                                    onChange={(value) => handleQuoteTypeSelect(value)}>
-                                    <ToggleButton id="single" value={"single"} size="sm">Citação</ToggleButton>
-                                    <ToggleButton id="multiple" value={"multiple"} size="sm">Diálogo</ToggleButton>
-                                </ToggleButtonGroup>
-                            </DropdownButton>
+                                    <DropdownButton variant="dark" menuVariant="dark" title="Nome" onSelect={handleSourceSelect}>
+                                        {SourceNames.map((item, index) => (
+                                            <DropdownItem eventKey={item.value} key={item.value}>{item.name}</DropdownItem>
+                                        ))
+                                        }
 
-                            <DropdownButton variant="dark" menuVariant="dark" title="Nome" onSelect={handleSourceSelect}>
-                                {SourceNames.map((item, index) => (
-                                    <DropdownItem eventKey={item.value} key={item.value}>{item.name}</DropdownItem>
-                                ))
-                                }
-                                
-                            </DropdownButton>
-                            <Button onClick={() => handleSortChange()}><i className="bi bi-sort-down-alt"></i></Button>
+                                    </DropdownButton>
+                                    <Button onClick={() => handleSortChange()}><i className="bi bi-sort-down-alt"></i></Button>
 
-                        </InputGroup>
-                    </Col>
-                </Row>
-                </>
-            )}            
-        </div>
-        
-        <SearchPath searchParams={searchParams}/>
+                                </InputGroup>
+                            </Col>
+                        </Row>
+                    </>
+                )}
+            </div>
+
+            <SearchPath searchParams={searchParams} />
         </>
     )
 }
