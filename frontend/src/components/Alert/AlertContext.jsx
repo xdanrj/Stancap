84a95883453f793186ded7f2122bceb8979c7659
xdanrj <xdanrj@gmail.com>
2024-04-28 15:50:26 -0300

    15h50

--- frontend/src/components/Alert/AlertContext.jsx
+++ frontend/src/components/Alert/AlertContext.jsx
@@ -1,28 +1,26 @@
 import React, { useContext, createContext } from "react"
 import { ToastContainer, toast } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css'
-import { renderToString } from 'react-dom/server';
-import parse from 'html-react-parser';
+import { renderToString } from 'react-dom/server'
+import parse from 'html-react-parser'
 
 const AlertContext = createContext()
 
 export function AlertProvider({ children }) {
-    // const notify = (message, duration=3500) => toast(
-    //     <> abc <br /> xyz </>,
-    //      { autoClose: duration })
 
     function notify(message, duration = 3500) {
         let splitMsg = message.split("\n")
-        console.log(splitMsg)
-        let msg = splitMsg.map((line, index) => (
-            <div key={index}>
-                {line}
-                {index !== message.split("\n").length - 1 && <br />}
+        const msg = (
+            <div>
+                {splitMsg.map((line, index) => (
+                    <div key={index}>
+                        {line}
+                        {index !== splitMsg.length - 1 && <br />}
+                    </div>
+                ))}
             </div>
-        ))
-        //todo: fazer com que msg seja um html normal pra funfar no toast
-        console.log(d(renderToString(msg)))
-        toast(renderToString(msg), { autoClose: duration })
+        )
+        toast(msg, { autoClose: duration })
     }
 
     return (
