f5e23fc32a2e981d28c24424a71b058d1de0fdc4
Danilo Carvalho <xdanrj@gmail.com>
2024-05-31 11:43:51 -0300

    11h40

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Stancap",
   define: {
-    'process.env': process.env,
-    plugins: [react()],
-    base: "/Stancap"
+    'process.env': process.env
   }
 })
