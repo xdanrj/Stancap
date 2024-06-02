50b5cf7c5a1fc45d8e941359704c3722bec08fb4
Danilo Carvalho <xdanrj@gmail.com>
2024-06-02 08:22:22 -0300

    8h20

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { createHashRouter, RouterProvider } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './index.css'
 
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: <App />,
-  }
-]);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
-);
+)
